f3438760a67f60123f8688cfee5b9adb9b30dd27
Bogdanov-Tsabert Timogor <mr.timogor@mail.ru>
2025-01-17 16:55:06 +0500

    Finished CatTest package

--- src/test/java/CatTest/GetFoodTest.java
+++ src/test/java/CatTest/GetFoodTest.java
@@ -4,16 +4,22 @@ import com.example.Cat;
 import com.example.Feline;
 
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 
 public class GetFoodTest {
 
     @Test
-    public void getFoodTest(){
-        Feline feline = new Feline(); // заменить на мок?
-        Cat testCat = new Cat(feline);
-        assertEquals("Мяу", testCat.getSound());
+    public void getFoodTest() throws Exception {
+        Feline felineMock = Mockito.mock(Feline.class);
+        Mockito.when(felineMock.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+
+        Cat cat = new Cat(felineMock);
+        assertEquals(List.of("Животные", "Птицы", "Рыба"), cat.getFood());
     }
 
 }
--- src/test/java/CatTest/GetSoundTest.java
+++ src/test/java/CatTest/GetSoundTest.java
@@ -3,8 +3,6 @@ package CatTest;
 import com.example.Cat;
 import com.example.Feline;
 
-import java.util.List;
-
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
@@ -12,10 +10,10 @@ import static org.junit.Assert.assertEquals;
 public class GetSoundTest {
 
     @Test
-    public void getFoodTest() throws Exception {
+    public void getSoundTest() {
         Feline feline = new Feline(); // заменить на мок?
-        Cat testCat = new Cat(feline);
-        assertEquals(List.of("Животные", "Птицы", "Рыба"), testCat.getFood());
+        Cat cat = new Cat(feline);
+        assertEquals("Мяу", cat.getSound());
     }
 
 }
